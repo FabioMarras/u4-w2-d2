cc8fc741e552deb5e3c8f94478bebf99aad98893
FabioMarras <fabio.marras@yahoo.com>
2023-10-10 14:23:57 +0200

    Initial commit

---
+++ src/Es1.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Es1 {
+    public static void main(String[] args) {
+        HashSet<String> ciccio = new HashSet<>();
+        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci un numero intero");
+        int nElements = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < nElements; i++){
+        System.out.println("Inserisci una parola");
+        String words = (input.nextLine());
+        ciccio.add(words);
+        }
+        for (String i : ciccio) {
+            System.out.println(i);
+        }
+    }
+}
